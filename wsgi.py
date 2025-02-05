9dc6d6c5e4b9944a36f2e51294a59bc805186e7d
Keshav <kj6560@gmail.com>
2025-02-05 18:17:50 +0530

    abc

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 import sys
 import logging
-import site
 import os
 
-# Add the virtual environment site-packages
-site.addsitedir('/var/www/shiwkesh/nudity/venv/lib/python3.10/site-packages')
 
 # Configure logging
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
